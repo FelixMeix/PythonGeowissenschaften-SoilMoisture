c5e3413ad46aa0641af92044201dce2b2d0436b0
Felix Meixner <felix.meixner@tuwien.ac.at>
2024-05-17 11:37:50 +0200

    first code

--- SoilMoisture.py
+++ SoilMoisture.py
@@ -1,3 +1,22 @@
+from ismn.interface import ISMN_Interface
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-#initial commit
+#Felix
+path = r"C:\Users\felix\OneDrive\Dokumente\TU Wien\Geowissenschaften-Python\Data_separate_files_header_20230517_20240517_11180_17tA_20240517.zip"
+#Bettina
+
+#Theresa
+
+
+#read in the data:
+ismn_data = ISMN_Interface(path, parallel=False)
+#print(ismn_data)
+
+#Select a Station:
+station = ismn_data['SCAN']['MccrackenMesa']
+
+
+
+i=0
